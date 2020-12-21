0473197344df2b893a8211db3cd45dc61459670b
hyperflow <hyperflow@kakao.com>
2020-12-22 03:30:40 +0900

    feat(todo): 스타일 추가 / pin-unpin 기능 추가

--- src/features/todo/Todo.tsx
+++ src/features/todo/Todo.tsx
@@ -1,36 +1,115 @@
-import { nanoid } from '@reduxjs/toolkit';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import styled, { CSSProperties } from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
-import { add, remove, returnTodo } from './todoSlice';
+import { add, remove, returnTodo, togglePin, ITodo } from './todoSlice';
+
+import {
+  AiFillPushpin,
+  AiOutlinePushpin,
+  AiOutlineDelete,
+} from 'react-icons/ai';
+
+const Pin = ({
+  pinFlag,
+  onClick,
+  style,
+}: {
+  pinFlag: boolean;
+  onClick: () => void;
+  style: CSSProperties;
+}) => {
+  if (pinFlag) {
+    return <AiFillPushpin style={style} onClick={onClick} />;
+  } else {
+    return <AiOutlinePushpin style={style} onClick={onClick} />;
+  }
+};
 
 const Todo = () => {
   const todo = useSelector(returnTodo);
   const dispatch = useDispatch();
 
   const [todoString, setTodoString] = useState('');
+  const [todoArray, setTodoArray] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    setTodoArray(
+      todo.slice().sort((a, b) => {
+        if (b.pin) return 1;
+        else if (a.pin) return -1;
+        else return 0;
+      })
+    );
+  }, [setTodoArray, todo]);
 
   return (
-    <>
-      <div>
-        <input
+    <div>
+      <h1>Simple Todo App.</h1>
+      <InputContainer>
+        <TodoInput
           placeholder="new todo string"
           onChange={e => setTodoString(e.target.value)}
           value={todoString}
         />
-        <button aria-label="add todo" onClick={() => dispatch(add(todoString))}>
+        <AddTodoButton
+          aria-label="add todo"
+          onClick={() => dispatch(add(todoString))}
+        >
           Add todo
-        </button>
-      </div>
-      <div>
-        {todo.map((value, index) => (
-          <p key={index}>
-            {value}
-            <button onClick={() => dispatch(remove(index))}>delete</button>
-          </p>
+        </AddTodoButton>
+      </InputContainer>
+      <TodoContainer>
+        {todoArray.map(value => (
+          <TodoParagraph key={value.id}>
+            <TodoSpan>
+              <Pin
+                pinFlag={value.pin}
+                onClick={() => dispatch(togglePin(value.id))}
+                style={{ marginRight: '0.5rem' }}
+              />
+              {value.content}
+            </TodoSpan>
+            <TodoSpan>
+              <AiOutlineDelete onClick={() => dispatch(remove(value.id))}>
+                delete
+              </AiOutlineDelete>
+            </TodoSpan>
+          </TodoParagraph>
         ))}
-      </div>
-    </>
+      </TodoContainer>
+    </div>
   );
 };
+
+const TodoInput = styled.input`
+  font-size: 2rem;
+`;
+
+const AddTodoButton = styled.button`
+  height: 2.75rem;
+
+  margin-left: 1rem;
+`;
+
+const InputContainer = styled.section`
+  display: flex;
+  align-items: center;
+`;
+
+const TodoContainer = styled.section`
+  display: flex;
+`;
+
+const TodoParagraph = styled.p`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TodoSpan = styled.span`
+  display: flex;
+  align-items: center;
+`;
 
 export default Todo;
--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -1,8 +1,14 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 
+export interface ITodo {
+  id: string;
+  content: string;
+  pin: boolean;
+}
+
 interface TodoState {
-  todo: string[];
+  todo: ITodo[];
 }
 
 const initialState: TodoState = {
@@ -13,16 +19,34 @@ export const todoSlice = createSlice({
   name: 'todo',
   initialState,
   reducers: {
-    add: (state, action: PayloadAction<string>) => {
-      state.todo.push(action.payload);
+    add: {
+      reducer: (state, action: PayloadAction<ITodo>) => {
+        state.todo.push(action.payload);
+      },
+      prepare: (content: string) => {
+        return {
+          payload: {
+            id: nanoid(),
+            content,
+            pin: false,
+          },
+        };
+      },
     },
-    remove: (state, action: PayloadAction<number>) => {
-      state.todo.splice(action.payload, 1);
+    remove: (state, action: PayloadAction<string>) => {
+      state.todo.splice(
+        state.todo.findIndex(item => item.id === action.payload),
+        1
+      );
+    },
+    togglePin: (state, action: PayloadAction<string>) => {
+      const todoState = state.todo.find(item => item.id === action.payload);
+      if (todoState) todoState.pin = !todoState?.pin;
     },
   },
 });
 
-export const { add, remove } = todoSlice.actions;
+export const { add, remove, togglePin } = todoSlice.actions;
 
 export const returnTodo = (state: RootState) => state.todo.todo;
 
